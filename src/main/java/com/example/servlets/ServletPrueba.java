afebfd902181ac02c60f0039c26335f23b843b29
Thomas Ortega <86833775+VintageStep@users.noreply.github.com>
2024-02-22 23:52:07 +0100

    first commit

---
+++ src/main/java/com/example/servlets/ServletPrueba.java
@@ -0,0 +1,24 @@
+package com.example.servlets;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class ServletPrueba extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Servlet dice hola desde .servlets</h1>");
+        out.println(LocalDateTime.now());
+        out.println("</body></html>");
+    }
+}
